ba5221dd0ebc725e88c515750632b5b516f57fbf
牧野俊輝 <lhaplus789@gmail.com>
2025-09-10 21:08:35 +0900

    init

---
+++ base_dir_project/rpc-api/src/main/java/jp/co/rpc/BaseDirProjectApplication.java
@@ -0,0 +1,13 @@
+package jp.co.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDirProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDirProjectApplication.class, args);
+	}
+
+}
